873c0a46299fbd4f3d3d8f1e35d096816097d02c
KKatiaa <katyakravchuk123@gmail.com>
2025-11-30 17:40:29 +0200

    comment

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
 
     // Construct the message
-    const message = `Application Environment: ${environment.toUpperCase()}\n\nToday's Date and Time: ${formattedDate}`;
+    const message = `Application Environment: ${environment.toUpperCase()}\n\nToday's Date and Time: ${formattedDate}       newwww`;
     
     console.log(`Github actions should only run on branch main`);
     
